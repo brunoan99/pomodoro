cae5ee7442c4793ed6447f38c6952d1a67f61419
Bruno Andrade <brunoan99@gmail.com>
2023-05-09 14:29:43 -0300

    feat: add Header to HomePage

--- src/main.tsx
+++ src/main.tsx
@@ -5,5 +5,6 @@ import { HomePage } from './pages/HomePage.tsx'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
+    <HomePage />
   </React.StrictMode>,
 )
