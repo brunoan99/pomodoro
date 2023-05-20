b8a82c26385207e851d6df7564310392beae9f3a
Bruno Andrade <brunoan99@gmail.com>
2023-05-20 13:28:47 -0300

    refactor: change some proportions

--- src/presenters/components/Button.tsx
+++ src/presenters/components/Button.tsx
@@ -1,13 +1,10 @@
 import { ReactElement } from "react"
 
-export const Button = ({
-    child,
-    onClick,
-  }: {
+export const Button = ({ child, onClick }: {
     child: ReactElement,
     onClick: React.MouseEventHandler<HTMLButtonElement>
   }) => (
-  <div className="bg-secondary-color min-w-[120px] w-[14vw] min-h-[65px] h-[8vh] shadow-lg rounded-3xl flex text-xl antialiased tracking-widest text-center align-middle justify-center hover:shadow-xl">
+  <div className="bg-secondary-color min-w-[120px] w-[12vw] min-h-[65px] shadow-lg rounded-3xl flex text-xl antialiased tracking-widest text-center align-middle justify-center hover:shadow-xl">
     <button className="self-center h-full w-full text-primary-font-color" onClick={onClick}>
       {child}
     </button>
--- src/presenters/components/Timer.tsx
+++ src/presenters/components/Timer.tsx
@@ -1,6 +1,6 @@
 export const Timer = ({ time, msg }: { time: string, msg: string }) => (
   <div className="flex justify-center items-center h-[63vh]">
-    <div className="bg-secondary-color min-w-[250px] w-[25vw] min-h-[250px] h-[25vw] rounded-full shadow-xl">
+    <div className="bg-secondary-color min-w-[250px] w-[30vw] min-h-[250px] h-[30vw] rounded-full shadow-xl">
       <div className="mt-[1vh] h-full w-full flex flex-col justify-center text-center align-middle">
         <p className="antialiased select-none tracking-[4px] text-3xl text-center text-primary-font-color">
           {time.split('').join(' ')}
