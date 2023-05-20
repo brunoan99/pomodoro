2cbe25cc79c19ce2a82351501b4ce564d6105240
Bruno Andrade <brunoan99@gmail.com>
2023-05-20 13:48:24 -0300

    refactor: change proportions

--- src/presenters/components/Button.tsx
+++ src/presenters/components/Button.tsx
@@ -4,7 +4,7 @@ export const Button = ({ child, onClick }: {
     child: ReactElement,
     onClick: React.MouseEventHandler<HTMLButtonElement>
   }) => (
-  <div className="bg-secondary-color min-w-[120px] w-[12vw] min-h-[65px] shadow-lg rounded-3xl flex text-xl antialiased tracking-widest text-center align-middle justify-center hover:shadow-xl">
+  <div className="bg-secondary-color min-w-[120px] w-[12vw] max-w-[200px] min-h-[65px] shadow-lg rounded-3xl flex text-xl antialiased tracking-widest text-center align-middle justify-center hover:shadow-xl">
     <button className="self-center h-full w-full text-primary-font-color" onClick={onClick}>
       {child}
     </button>
