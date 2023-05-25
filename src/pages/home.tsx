3e22f85de5e2421e7349d3efac9a126960bf3d03
Bruno Andrade <brunoan99@gmail.com>
2023-05-25 00:22:56 -0300

    fix: fix build error

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -32,7 +32,6 @@ export default function HomePage() {
             />}
             action={
               <SwitchThemeButton
-                onClick={() => {}}
               />
             }
           />,
